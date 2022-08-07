e3de566129bbde0b043c15129a54454812c1013a
Viacheslav <davidov.ve@yandex.ru>
2022-08-07 08:26:51 +0300

    Task 1 project

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите первое чилсо: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе чилсо: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write("Большее число: "); 
+    Console.WriteLine(numberA);
+    Console.Write("Меньшее число: "); 
+    Console.WriteLine(numberB);
+}
+
+if (numberB > numberA)
+{
+    Console.Write("Большее число: "); 
+    Console.WriteLine(numberB);
+    Console.Write("Меньшее число: "); 
+    Console.WriteLine(numberA);
+}
+
+if (numberB == numberA)
+{
+    Console.Write("Извините, числа равны. Повторите ввод!"); 
+}
