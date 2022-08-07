c7f0468afcc9b54fe9fe4522f628c0d3b56d4509
Viacheslav <davidov.ve@yandex.ru>
2022-08-07 08:30:19 +0300

    Task 4  project

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое чилсо: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе чилсо: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье чилсо: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+//if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
