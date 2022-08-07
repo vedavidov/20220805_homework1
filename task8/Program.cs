7cb4484298e4902cdf81fc74abeb23fbdf80f4a4
Viacheslav <davidov.ve@yandex.ru>
2022-08-07 13:00:31 +0300

    Task 8 project

--- task8/Program.cs
+++ task8/Program.cs
@@ -2,9 +2,8 @@
 
 Console.WriteLine("Введите значение чилса N: ");
 int N = int.Parse(Console.ReadLine());
-
+Console.WriteLine("Чётные числа диапазона от 1 до N: ");
 for (int i = 1; i <= N; i = i + 1)
     if (i % 2 == 0)   
 
 Console.WriteLine(i);
-Console.WriteLine("*Выше указаны чётные числа диапазона от 1 до N");
