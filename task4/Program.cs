fc5ec2989caa4740e8bf067ccaf5c78e82db3341
Viacheslav <davidov.ve@yandex.ru>
2022-08-07 12:55:22 +0300

    Task 4 project

--- task4/Program.cs
+++ task4/Program.cs
@@ -12,5 +12,7 @@ int max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 
-Console.Write("max = ");
-Console.WriteLine(max);
+//Console.Write("max = ");
+//Console.WriteLine(max);
+
+Console.WriteLine($"Максимальное число = {max}");
